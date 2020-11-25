8e539279232acfbc3a1e6d7eec86aeec40dbb3bf
Jared Salazar Martinez <33723377+TateTipycsnow@users.noreply.github.com>
2020-11-25 00:05:07 -0600

    2nd AFD

--- ventanas.js
+++ ventanas.js
@@ -8,8 +8,8 @@ firstButton.addEventListener('click', (event) => {
     const afd1er = path.join('file://', __dirname, '/windows/Primer AFD/1erAFD.html');
 
     let ventana1 = new BrowserWindow({
-        width: 680,
-        height: 680,
+        width: 690,
+        height: 660,
         webPreferences: {
             nodeIntegration: true,
             enableRemoteModule: true,
@@ -26,8 +26,8 @@ secondButton.addEventListener('click', (event) => {
     const afd2nd = path.join('file://', __dirname, '/windows/Segundo AFD/2ndAFD.html');
 
     let ventana2 = new BrowserWindow({
-        width: 680,
-        height: 680,
+        width: 820,
+        height: 530,
         webPreferences: {
             nodeIntegration: true,
             enableRemoteModule: true,
